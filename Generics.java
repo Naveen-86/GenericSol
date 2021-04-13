de7a398fac3f039ec42fb0c3a35cb3b4714eb64f
Naveen <naveentruly86@gmail.com>
2021-04-13 13:52:06 +0530

    Added: Max Float Value

--- Generics.java
+++ Generics.java
@@ -1,5 +1,5 @@
 public class Generics {
-    // determines the largest of three comparable integers
+    // determines the largest of three comparable float values
     public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
         T max = x; // assume x is initially the largest
 
@@ -9,11 +9,11 @@ public class Generics {
         if (z.compareTo(max) > 0)
             max = z; // z is the largest
 
-        return max; // return the largest integer
+        return max; // return the largest float value
     } // end method maximum
 
     public static void main(String[] args) {
-        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 5, 8, 10, maximum(5, 8, 10));
+        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 7.7, 8.8, maximum(6.6, 7.7, 8.8));
     }
 
 }
