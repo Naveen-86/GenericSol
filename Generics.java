bee9798285961a0a6e05f1ebbc5e27b2e407fe59
Naveen <naveentruly86@gmail.com>
2021-04-13 14:50:38 +0530

    Added: Refactor three into one

--- Generics.java
+++ Generics.java
@@ -1,5 +1,5 @@
 public class Generics {
-    // determines the largest of three comparable String values
+    // determines the largest of three comparable objects
     public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
         T max = x; // assume x is initially the largest
 
@@ -9,11 +9,15 @@ public class Generics {
         if (z.compareTo(max) > 0)
             max = z; // z is the largest
 
-        return max; // return the largest String value
+        return max; // return the largest object
     } // end method maximum
 
     public static void main(String[] args) {
-        System.out.printf("Maximum of %s, %s and %s is %s\n\n", "Apple", "Banana", "Peach", maximum("Apple", "Banana", "Peach"));
+         System.out.printf("Maximum of %d, %d and %d is %d\n\n", 5, 8, 10, maximum(5, 8, 10));
+
+         System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 7.7, 8.8, maximum(6.6, 7.7, 8.8));
+
+         System.out.printf("Maximum of %s, %s and %s is %s\n\n", "Apple", "Banana", "Peach", maximum("Apple", "Banana", "Peach"));
     }
 
 }
